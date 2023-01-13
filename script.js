9bf76e0c52f2247ea2a5e6b5d8cda1348a0cb090
Khirby Calma <95603154+KhirbyCalma@users.noreply.github.com>
2023-01-13 14:30:16 -0800

    Implement drawing feature

--- script.js
+++ script.js
@@ -1,11 +1,33 @@
 const gridContainer = document.getElementById('grid-container');
+let mouseDown = false;
 
 function createGrid(width){
     gridContainer.style.gridTemplateColumns = `repeat(${width}, 1fr)`;
     for (let i = 0; i < width * width; i++){
         const gridBox = document.createElement('div');
         gridBox.classList.add('grid-box');
+        gridBox.addEventListener('mouseover', changeColor);
+        gridBox.addEventListener('mousedown', (e) => {
+            updateMouseDown(e);
+            changeColor(e);
+        });
+        gridBox.addEventListener('mouseup', updateMouseDown);
         gridContainer.appendChild(gridBox);
+    }
+}
+
+function updateMouseDown(e){
+    if (e.type === 'mousedown'){
+        mouseDown = true;
+    }
+    else{
+        mouseDown = false;
+    }
+}
+
+function changeColor(e){
+    if (mouseDown || (e.type === 'mouseover' && mouseDown)){
+        e.target.style.backgroundColor = 'red';
     }
 }
 
