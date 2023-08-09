30087d23349914c38fc9f77ae0f9a0309d0fa9ba
Khirby Calma <95603154+KhirbyCalma@users.noreply.github.com>
2023-08-08 20:37:55 -0700

    Implement rainbow drawing mode

--- script.js
+++ script.js
@@ -29,12 +29,15 @@ colorPicker.addEventListener('change', (event) => {
 
 // DRAW, ERASE, AND CLEAR ELEMENTS
 const drawMode = document.getElementById('draw-mode');
+const rainbowMode = document.getElementById('rainbow-mode');
 const eraserMode = document.getElementById('eraser-mode');
 const clearMode = document.getElementById('clear-mode');
 // DRAW, ERASE, AND CLEAR VARIABLES
 const COLOR_WHITE = "#FFFFFF";
+let currentMode = "DRAW";
 // DRAW, ERASE, AND CLEAR LISTENERS
 drawMode.addEventListener('click', changeToDrawMode);
+rainbowMode.addEventListener('click', changeToRainbowMode);
 eraserMode.addEventListener('click', changeToEraseMode);
 clearMode.addEventListener('click', clearGrid);
 // DRAW, ERASE, AND CLEAR STYLING
@@ -82,7 +85,18 @@ function updateMouseDown(e){
 
 function draw(e){
     if (mouseDown || (e.type === 'mouseover' && mouseDown)){
-        e.target.style.backgroundColor = userColor;
+        switch (currentMode) {
+            case "DRAW":
+                e.target.style.backgroundColor = userColor;
+                break;
+            case "RAINBOW":
+                e.target.style.backgroundColor = getRandomColor();
+                break;
+            case "ERASE":
+                e.target.style.backgroundColor = userColor;
+                break;
+        }
+        
     }
 }
 
@@ -90,18 +104,36 @@ function draw(e){
 // COLOR PICKER FUNCTIONS
 function getCurrentColor(){ return colorPicker.value; }
 function changeColor(color){ userColor = color; }
+// random RGB color generator: https://www.slingacademy.com/article/how-to-generate-random-color-in-javascript/#:~:text=To%20generate%20a%20random%20RGB,string%20with%20commas%20and%20parentheses.
+function getRandomColor() {
+    let r = Math.floor(Math.random() * 256); // Random between 0-255
+    let g = Math.floor(Math.random() * 256); // Random between 0-255
+    let b = Math.floor(Math.random() * 256); // Random between 0-255
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
 
 
 // DRAW, ERASE, AND CLEAR MODES
 function changeToDrawMode() {
     userColor = getCurrentColor();
+    currentMode = "DRAW";
     drawMode.classList.add('selected-mode');
+    rainbowMode.classList.remove('selected-mode');
+    eraserMode.classList.remove('selected-mode');
+}
+
+function changeToRainbowMode() {
+    currentMode = "RAINBOW";
+    drawMode.classList.remove('selected-mode');
+    rainbowMode.classList.add('selected-mode');
     eraserMode.classList.remove('selected-mode');
 }
 
 function changeToEraseMode(){
     userColor = COLOR_WHITE;
+    currentMode = "ERASE";
     drawMode.classList.remove('selected-mode');
+    rainbowMode.classList.remove('selected-mode');
     eraserMode.classList.add('selected-mode');
 }
 
