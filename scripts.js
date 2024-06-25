89ad9790e37f72e49b2fdadaa20e4a814bebd372
KhirbyCalma <95603154+KhirbyCalma@users.noreply.github.com>
2024-06-24 23:31:33 -0700

    Add grid size changer logic

--- scripts.js
+++ scripts.js
@@ -1,3 +1,52 @@
+// get grid container to manipulate
+const gridContainer = document.getElementById("grid-container");
+function buildGrid(gridSize) {
+    // update grid size text output
+    gridSizeTextOutput.textContent = `${gridSize} x ${gridSize}`;
+    // update grid css to dynamically adapt to grid sizes
+    gridContainer.style.gridTemplateColumns = `repeat(${gridSize}, 1fr)`;
+    // make 16 divs to act as gridboxes
+    for (let i = 0; i < (gridSize * gridSize); i++) {
+        // make individual div to act as a grid box 
+        const gridBox = document.createElement("div");
+        // add class to identify grid box
+        gridBox.classList.add("grid-box");
+        /* 
+            change background color when mouse is down AND mouse is over the element
+            cannot listen to multiple events so that is why multiple event listeners
+            mousedown instead of click because click requires mouse down and mouse up 
+        */
+        gridBox.addEventListener("mousedown", () => {
+            gridBox.style.backgroundColor = drawingColor;
+        });
+        // listening to both mouseover AND mousedown
+        gridBox.addEventListener("mouseover", () => {
+            if (mouseDown) {
+                gridBox.style.backgroundColor = drawingColor;
+            }
+        });
+        // add to grid container
+        gridContainer.appendChild(gridBox);
+    }
+}
+
+// get grid size slider to get grid size information from
+const gridSizeSelector = document.getElementById("grid-size-selector");
+// initialize global grid size
+let gridSize = gridSizeSelector.value;
+// listen for any grid size changes to update global grid size
+gridSizeSelector.addEventListener("input", () => {
+    gridSize = gridSizeSelector.value;
+    // reset grid
+    while (gridContainer.hasChildNodes()) {
+        gridContainer.removeChild(gridContainer.lastChild)
+    }
+    buildGrid(gridSize);
+});
+
+// get grid size text output
+const gridSizeTextOutput = document.getElementById("grid-size-text-output");
+
 // get color picker to get color information from
 const colorPicker = document.getElementById("color-picker");
 // initialize global drawing color
@@ -13,7 +62,7 @@ colorPicker.addEventListener("input", () => {
 
 // get eraser to see if user wants to reset grid box color to white
 const eraser = document.getElementById("eraser");
-// list for user clicking button to initiate eraser mode
+// listen for user clicking button to initiate eraser mode
 eraser.addEventListener("click", () => {
     drawingColor = "white";
 });
@@ -24,28 +73,4 @@ let mouseDown = false;
 body.addEventListener("mousedown", () => mouseDown = true);
 body.addEventListener("mouseup", () => mouseDown = false);
 
-// get grid container to manipulate
-const gridContainer = document.getElementById("grid-container");
-// make 16 divs to act as gridboxes
-for (let i = 0; i < 16; i++) {
-    // make individual div to act as a grid box 
-    const gridBox = document.createElement("div");
-    // add class to identify grid box
-    gridBox.classList.add("grid-box");
-    /* 
-        change background color when mouse is down AND mouse is over the element
-        cannot listen to multiple events so that is why multiple event listeners
-        mousedown instead of click because click requires mouse down and mouse up 
-    */
-    gridBox.addEventListener("mousedown", () => {
-        gridBox.style.backgroundColor = drawingColor;
-    });
-    // listening to both mouseover AND mousedown
-    gridBox.addEventListener("mouseover", () => {
-        if (mouseDown) {
-            gridBox.style.backgroundColor = drawingColor;
-        }
-    });
-    // add to grid container
-    gridContainer.appendChild(gridBox);
-}
+document.addEventListener("DOMContentLoaded", () => buildGrid(gridSize));
